4e594b859bfe45404213d04f8ea09e5c3e2228bf
MukhriddinMike <miwady@mail.ru>
2019-08-22 13:02:53 +0900

    Add project files.

---
+++ Presige.BizTest/ActorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prestige.Biz;
+
+namespace Presige.BizTest
+{
+    [TestClass]
+    public class ActorTest
+    {
+        [TestMethod]
+        public void TestGetOccupatin()
+        {
+            // Arrange
+            var currentActor = new Actor();
+            var expected = "Actor";
+
+            // Act
+          
+            string result = currentActor.GetOccupation();
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Presige.BizTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Presige.BizTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Presige.BizTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f66371e6-725a-4d3b-80e0-6207aa9a47fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
